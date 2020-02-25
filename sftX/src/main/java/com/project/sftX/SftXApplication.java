0470dab810cc97c53600c71145858f451ffb416c
EdgarACarneiro <edgarcarneiro1306@gmail.com>
2020-02-25 15:38:27 +0000

    Added base project as well as gitignore

---
+++ sftX/src/main/java/com/project/sftX/SftXApplication.java
@@ -0,0 +1,13 @@
+package com.project.sftX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SftXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SftXApplication.class, args);
+	}
+
+}
---
+++ sftX/src/test/java/com/project/sftX/SftXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sftX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SftXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
