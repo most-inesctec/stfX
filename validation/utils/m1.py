fddf39d1aae055394fa4d85f3dd0348dea6dd690
EdgarACarneiro <edgarcarneiro1306@gmail.com>
2020-05-10 17:06:31 +0100

    Added surveyor's formula

--- validation/utils/m1.py
+++ validation/utils/m1.py
@@ -7,36 +7,31 @@ https://towardsdatascience.com/the-concave-hull-c649795c0f0f
 https://en.wikipedia.org/wiki/Alpha_shape
 """
 
-
-def find_splits(array1: list, array2: list) -> list:
-    """Find the split points of the given array of events"""
-    keys = set()
-    for event in array1:
-        keys.add(event["temporalRange"][0])
-        keys.add(event["temporalRange"][1])
-    for event in array2:
-        keys.add(event["temporalRange"][0])
-        keys.add(event["temporalRange"][1])
-
-    return list(sorted(keys))
+X_COORDINATE = 0
+Y_COORDINATE = 1
 
 
-def subdivide(events: list, splits: list) -> list:
-    """Split the given events to match the keys in the splits list"""
-    formatted_events = []
+def area(x, y):
+    n = len(x)
+    s = 0.0
+    for i in range(-1, n-1):
+        s += x[i]*y[i+1] - y[i]*x[i+1]
+    return 0.5*s
 
-    for i in range(len(splits)-1):
-        formatted_event = {}
-        formatted_event["temporalRange"] = [splits[i], splits[i + 1]]
-        # Get the events of the enclosing event (it must always be one)
-        for event in events:
-            tr = event["temporalRange"]
-            if tr[0] <= splits[i] and tr[1] >= splits[i+1]:
-                formatted_event["events"] = event["events"]
-        # Adding the formatted event to the return result
-        formatted_events.append(formatted_event)
 
-    return formatted_events
+# def surveyor_formula(polygon: list) -> float:
+#     return area([el[0] for el in polygon], [el[1] for el in polygon])
+
+
+def surveyor_formula(polygon: list) -> float:
+    """Find the area of the given polygon using the surveyor formula"""
+    area = 0
+
+    for i in range(-1, len(polygon)-1):
+        area += polygon[i][X_COORDINATE] * polygon[i+1][Y_COORDINATE] -\
+            polygon[i][Y_COORDINATE] * polygon[i+1][X_COORDINATE]
+
+    return abs(area / 2)
 
 
 def extract_transformations(events: list) -> list:
@@ -46,69 +41,63 @@ def extract_transformations(events: list) -> list:
 
 def apply_m1(results: list, expected_results: list) -> float:
     """Apply the metric M2 and obtain its result, between 0 and 100"""
-    splits = find_splits(results, expected_results)
-
-    formatted_results = subdivide(results, splits)\
-        if len(results) < len(splits) - 1\
-        else results
-    formatted_expected = subdivide(expected_results, splits)\
-        if len(expected_results) < len(splits) - 1\
-        else expected_results
-
-    # Computing the weighted average distance
-    w_avg_distance = 0
-    for e1, e2 in zip(formatted_results, formatted_expected):
-        t_range = e1["temporalRange"]
-        A = set(extract_transformations(e1["events"]))
-        B = set(extract_transformations(e2["events"]))
-        w_avg_distance += (len(A-B) + len(B-A)) * (t_range[1] - t_range[0])
-    w_avg_distance /= splits[len(splits)-1] - splits[0]
-
-    # Diving by worst-case scenario an return as percent
-    return (1 - (w_avg_distance / NUMBER_MAX_TRANSFORMATIONS)) * TO_PERCENT
+    return None
 
 
-class TestM2(unittest.TestCase):
+class TestM1(unittest.TestCase):
 
-    def test_equal(self):
-        results = [{
-            "events": [
-                {"type": "TRANSLATION"},
-                {"type": "ROTATION"},
-                {"type": "SCALE"}
-            ],
-            "temporalRange": [0, 100]
-        }, {
-            "events": [
-                {"type": "TRANSLATION"}
-            ],
-            "temporalRange": [100, 120]
-        }, {
-            "events": [
-                {"type": "ROTATION"},
-                {"type": "SCALE"}
-            ],
-            "temporalRange": [120, 150]
-        }]
-        expected_results = [{
-            "events": [
-                {"type": "TRANSLATION"},
-                {"type": "ROTATION"}
-            ],
-            "temporalRange": [0, 120]
-        }, {
-            "events": [
-                {"type": "ROTATION"},
-                {"type": "SCALE"}
-            ],
-            "temporalRange": [120, 150]
-        }]
+    def test_area(self):
+        square = [
+            [1, 1],
+            [1, -1],
+            [-1, -1],
+            [-1, 1],
+            # [0, 0],
+            # [0, 2],
+            # [2, 2],
+            # [2, 0]
+        ]
+        self.assertEqual(surveyor_formula(square), 4)
 
-        self.assertEqual(apply_m1(results, expected_results), 100)
+    # def test_equal(self):
+    #     results = [{
+    #         "events": [
+    #             {"type": "TRANSLATION"},
+    #             {"type": "ROTATION"},
+    #             {"type": "SCALE"}
+    #         ],
+    #         "temporalRange": [0, 100]
+    #     }, {
+    #         "events": [
+    #             {"type": "TRANSLATION"}
+    #         ],
+    #         "temporalRange": [100, 120]
+    #     }, {
+    #         "events": [
+    #             {"type": "ROTATION"},
+    #             {"type": "SCALE"}
+    #         ],
+    #         "temporalRange": [120, 150]
+    #     }]
+    #     expected_results = [{
+    #         "events": [
+    #             {"type": "TRANSLATION"},
+    #             {"type": "ROTATION"}
+    #         ],
+    #         "temporalRange": [0, 120]
+    #     }, {
+    #         "events": [
+    #             {"type": "ROTATION"},
+    #             {"type": "SCALE"}
+    #         ],
+    #         "temporalRange": [120, 150]
+    #     }]
 
-    def test_different(Self):
+    #     self.assertEqual(apply_m1(results, expected_results), 100)
 
-        self.assertEqual(apply_m1(), 50)
+    # def test_different(Self):
+
+    #     self.assertEqual(apply_m1(), 50)
 
 
 if __name__ == '__main__':
