e1380738a3451695a3190c41740ba67f395ee325
EdgarACarneiro <edgarcarneiro1306@gmail.com>
2020-05-24 05:25:26 +0100

    Set tup PSR mock middleware

---
+++ validation/psr-mock/middleware.js
@@ -0,0 +1,22 @@
+module.exports = (req, _, next) => {
+    dataset = req.body
+    last_y_x = dataset[dataset.length - 1].y[0][0]
+
+    switch (last_y_x) {
+        case 182.67929:
+            req.url = '/multiple'
+            break
+        case 972.22461:
+            req.url = '/sequence'
+            break
+    }
+
+    req.originalUrl = req.url
+    req._parsedUrl.pathname = req.url
+    req._parsedUrl.path = req.url
+    req._parsedUrl.href = req.url
+    req._parsedUrl._raw = req.url
+    req.method = 'GET'
+
+    next()
+}
