f1caa26c1d0e3d259d1c1a992a6451823baf5ea1
EdgarACarneiro <edgarcarneiro1306@gmail.com>
2020-05-26 03:32:33 +0100

    Fixed minor bug on mocked rotations

--- validation/experiment_with_mock.sh
+++ validation/experiment_with_mock.sh
@@ -1,17 +1,25 @@
 #!/bin/sh
 . venv/bin/activate
-# First mock
+
+# Mock non multiple sequence test
 echo "$(tput setaf 4)::: mocked_bad_non_multiple_sequence_test :::"
 python validate.py -d experiments/bad_non_multiple_sequence_test -e http://localhost:8080
 mv experiments/bad_non_multiple_sequence_test/m1.png results/mocked_bad_non_multiple_sequence_test_m1.png
 mv experiments/bad_non_multiple_sequence_test/result.json results/mocked_bad_non_multiple_sequence_test.json
-# Second mock
+
+# Mock single multiple
+# T1
 echo "$(tput setaf 4)::: mocked_awful_single_multiple_transformations_test :::"
 python validate.py -d experiments/awful_single_multiple_transformations_test -e http://localhost:8080
 mv experiments/awful_single_multiple_transformations_test/m1.png results/mocked_awful_single_multiple_transformantions_test_m1.png
 mv experiments/awful_single_multiple_transformations_test/result.json results/mocked_awful_single_multiple_transformations_test.json
-# Third mock
+# T2
 echo "$(tput setaf 4)::: mocked_bad_single_multiple_transformations_test :::"
 python validate.py -d experiments/bad_single_multiple_transformations_test -e http://localhost:8080
 mv experiments/bad_single_multiple_transformations_test/m1.png results/mocked_bad_single_multiple_transformantions_test_m1.png
 mv experiments/bad_single_multiple_transformations_test/result.json results/mocked_bad_single_multiple_transformations_test.json
+# T3
+echo "$(tput setaf 4)::: mocked_single_multiple_transformations_test :::"
+python validate.py -d experiments/single_multiple_transformations_test -e http://localhost:8080
+mv experiments/single_multiple_transformations_test/m1.png results/mocked_single_multiple_transformantions_test_m1.png
+mv experiments/single_multiple_transformations_test/result.json results/mocked_single_multiple_transformations_test.json
